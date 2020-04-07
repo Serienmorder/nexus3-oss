38aac4b4ea372859fd860923fb5a6d8a66bd6f6b
Serienmorder <pyro985@gmail.com>
2020-04-07 15:16:10 -0700

    Initial Add of assets, components, and ansible component framework

---
+++ module_utils/nexus/asset.py
@@ -0,0 +1,51 @@
+class Asset:
+    def __init__(self, **kwargs):
+        self.download_url = kwargs['downloadUrl']
+        self.path = kwargs['path']
+        self.id = kwargs['id']
+        self.repository = kwargs['repository']
+        self.format = kwargs['yum']
+        checksum = {}
+        for x in kwargs['checksum']:
+            checksum.update(x)
+
+    @property
+    def download_url(self):
+        return self.__download_url
+
+    @download_url.setter
+    def download_url(self, value):
+        self.__download_url = value
+
+    @property
+    def path(self):
+        return self.__path
+
+    @path.setter
+    def path(self, value):
+        self.__path = value
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, value):
+        self.__id = value
+
+    @property
+    def repository(self):
+        return self.__repository
+
+    @repository.setter
+    def repository(self, value):
+        self.__repository = value
+
+    @property
+    def format(self):
+        return self.__format
+
+    @format.setter
+    def format(self, value):
+        self.__format = value
+
---
+++ module_utils/nexus/component.py
@@ -0,0 +1,78 @@
+class Component:
+    def __init__(self, **kwargs):
+        if 'id' in kwargs:
+            self.id = kwargs['id']
+        else:
+            self.id = None
+        if 'repository' in kwargs:
+            self.repository = kwargs['repository']
+        else:
+            self.repository = None
+        if 'format' in kwargs:
+            self.format = kwargs['format']
+        else:
+            self.format = None
+        if 'group' in kwargs:
+            self.group = kwargs['group']
+        else:
+            self.group = None
+        if 'name' in kwargs:
+            self.name = kwargs['name']
+        else:
+            self.name = None
+        if 'version' in kwargs:
+            self.path = kwargs['version']
+        else:
+            self.path = None
+        if 'assets' in kwargs:
+            self.assets = {}
+            for x in kwargs['assets']:
+                self.assets.update(x)
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, value):
+        self.__id = value
+
+    @property
+    def repository(self):
+        return self.__repository
+
+    @repository.setter
+    def repository(self, value):
+        self.__repository = value
+
+    @property
+    def format(self):
+        return self.__format
+
+    @format.setter
+    def format(self, value):
+        self.__format = value
+
+    @property
+    def group(self):
+        return self.__group
+
+    @group.setter
+    def group(self, value):
+        self.__group = value
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @property
+    def version(self):
+        return self.__version
+
+    @version.setter
+    def version(self, value):
+        self.__version = value
